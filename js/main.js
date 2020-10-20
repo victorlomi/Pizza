8d2165c0bf83a0a136f0fbfc759ab1bede4ee5c4
victorlomi <victorlominyo@gmail.com>
2020-10-20 03:56:43 +0300

    add pizza object that represents an order

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// Pizza object that handles storing qll 
+// relevant info(size, crust, toppings, etc)
+function Pizza(size, crust) {
+    this.size = size;
+    this.crust = crust;
+    this.price = 0;
+    this.toppings = [];
+    this.orders = 1;
+}
+
+Pizza.prototype.calculateSize = function() {
+    // This function returns price based on size
+    switch (this.size) {
+        case "small":
+            return 500;
+            break;
+        case "medium":
+            return 1000;
+            break;
+        case "large":
+            return 2000;
+        default:
+            break;
+    }
+}
+
+Pizza.prototype.calculateToppings = function() {
+    // This function returns price based on toppings
+    // toppings are the same in price for now
+    return this.toppings.length * 50;
+}
+
+Pizza.prototype.calculateCrust = function() {
+    // This function returns price based on crust
+    switch (this.crust) {
+        case "stuffed":
+            return 200;
+            break;
+        case "deep":
+        case "pan":
+            return 150;
+            break;
+        case "crispy":
+        case "flatbread":
+            return 100;
+            break;
+        default:
+            break;
+    }
+}
+
+
+Pizza.prototype.getPrice = function() {
+    // This function returns the price of the pizza
+    // depending on it's size, crust, toppings,
+    // and the number of orders
+    let finalPrice = 0;
+    finalPrice += this.calculateSize();
+    finalPrice += this.calculateToppings();
+    finalPrice += this.calculateCrust();
+    finalPrice *= this.orders;
+    return finalPrice;
+};
+
+let sm = new Pizza("small", "stuffed");
+console.log(sm.getPrice());
